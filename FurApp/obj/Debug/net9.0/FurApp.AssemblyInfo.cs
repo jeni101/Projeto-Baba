9362a4123c1b1b5919e4c6fb57a7ca1009b22250
Jenifer gomes <jenifer.c.gomes@gmail.com>
2025-04-25 14:13:25 -0300

    0.0.0.0.0.0.0.0.005.1

--- FurApp/obj/Debug/net9.0/FurApp.AssemblyInfo.cs
+++ FurApp/obj/Debug/net9.0/FurApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FurApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1c1f7f989b1c838b326228e86c4fbcbaff4682a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecfd7398dfc479b048707e93d7c689e5fd3ecb52")]
 [assembly: System.Reflection.AssemblyProductAttribute("FurApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FurApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
