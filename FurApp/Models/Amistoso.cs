a56c079d4ff054427f2eab9723017ce8b64b4940
Jenifer gomes <jenifer.c.gomes@gmail.com>
2025-04-25 19:04:46 -0300

    0.0.0.0.0.0.0.0.010.1

--- FurApp/Models/Amistoso.cs
+++ FurApp/Models/Amistoso.cs
@@ -9,11 +9,15 @@ namespace AmistosoApp
         // dependendo do quanto de jogadores tiver da p fazer um random p completar time.
         // mas e se o cara q entrar no time n quiser jg ou n tiver disponivel? criar uma verificacao?
         public List<string> Participantes {get; set;}
+        public DateTime Data { get; set; } // estrutura q representa data e hora expecifica p amistoso
+        public string Local { get; set; }
         public Placar Placar { get; set; }
 
-        public Amistoso(List<string> participantes, Placar placar)
+        public Amistoso(List<string> participantes, DateTime data , string local, Placar placar)
         {
             Participantes = participantes;
+            Data = data;
+            Local = local;
             Placar = placar;
         }
 
--- FurApp/obj/Debug/net9.0/FurApp.AssemblyInfo.cs
+++ FurApp/obj/Debug/net9.0/FurApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FurApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+972718a71e2006b799cfac5ac323b41692be0b65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bc7f71477a89be79bd7810f148522c2b5f0c2ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("FurApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FurApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
