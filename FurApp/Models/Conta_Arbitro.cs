3306f3bc584ed5e7e6f91ac9e5d4c305fbff05e6
Luix <lugasoal@gmail.com>
2025-05-02 10:57:11 -0300

    00.3.0.0.0.0.0.005

--- FurApp/Models/Conta_Arbitro.cs
+++ FurApp/Models/Conta_Arbitro.cs
@@ -1,35 +1,30 @@
-using System;
 using ContaUsuarioApp;
 
 namespace ContaArbitroApp
 {
-    public class ContaArbitro : ContaUsuario
+    public class Conta_Arbitro : ContaUsuario
     {
         // mesma observacao da conta tecnico.
         public string Jogos {get; set;}
         public string Partidas {get; set;}
 
-        public ContaArbitro(string nome, string senha, int idade, float? saldo = null, string? interesses = null, string? amistosos = null, string? jogos = null, string? partidas = null)
+        public Conta_Arbitro(string nome, string senha, int idade, float? saldo = null, string? interesses = null, string? amistosos = null, string? jogos = null, string? partidas = null)
             : base(nome, senha, idade, saldo ?? 0, interesses ?? string.Empty, amistosos ?? string.Empty)
             {
                 Jogos = jogos ?? string.Empty;
                 Partidas = partidas ?? string.Empty;
             }
-        
-        
          //jogo obs - mesmas funcoes presentes na classe tecnico com execao do exibir, reciclagem de codigo?
-        public void CriarJogo(){}
-        public void ExibirJogo(){}
-        public void EditarJogo(){}
-        public void DeletarJogo(){}
+        public void Criar_Novo_Jogo(){}
+        public void Exibir_Jogo(){}
+        public void Editar_Jogo(){}
+        public void Deletar_Jogo(){}
 
         //partidas obs - mesmas funcoes presentes na classe tecnico com execao do exibir, reciclagem de codigo?
-        public void CriarPartidas(){}
-        public void ExibirPartidas(){}
-        public void EditarPartidas(){}
-        public void DeletarPartidas(){}
-        public void EncerrarPartidas(){}
-
-
+        public void Criar_Partidas(){}
+        public void Exibir_Partidas(){}
+        public void Editar_Partidas(){}
+        public void Deletar_Partidas(){}
+        public void Encerrar_Partidas(){}
     }
 }
