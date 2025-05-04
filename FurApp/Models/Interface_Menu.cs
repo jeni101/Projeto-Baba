c1d0ba40aff997cddd980247e6ccc6beb1e1db27
Luix <lugasoal@gmail.com>
2025-05-03 21:47:48 -0300

    00.3.0.0.0.0.0.006

---
+++ FurApp/Models/Interface_Menu.cs
@@ -0,0 +1,21 @@
+namespace Interface_Menu
+{
+    public static class Interface_Inicial
+    {
+        public static void Print_01()
+        {
+            Console.WriteLine(@" ______                      _____  _____   ");
+            Console.WriteLine(@"|  ____|               /\   |  __ \|  __ \  ");
+            Console.WriteLine(@"| |__ _   _ _ __ _ __ /  \  | |__) | |__) | ");
+            Console.WriteLine(@"|  __| | | | '__| '__/ /\ \ |  ___/|  ___/  ");
+            Console.WriteLine(@"| |  | |_| | |  | | / ____ \| |    | |      ");
+            Console.WriteLine(@"|_|   \__,_|_|  |_|/_/    \_\_|    |_|      ");
+        }
+        public static void Print_02()
+        {
+            Console.WriteLine("");
+            return;
+        }
+
+    }
+}
