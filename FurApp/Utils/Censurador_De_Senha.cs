32f1219bec7c03fa1f0e42a8cf3b9d11baac88e7
Gustavo-Vieira-Unasp <fonseca_vieira@educadventista.org>
2025-05-18 15:10:06 -0300

    00.5.0.0.0.0.0.003

--- FurApp/Utils/Censurador_De_Senha.cs
+++ FurApp/Utils/Censurador_De_Senha.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Security.Cryptography;
 
-namespace Utils.CensuradorDeSenha
+namespace Utils.Pelase.CensuradorDeSenha
 {
     public static class CensuradorDeSenha
     {
