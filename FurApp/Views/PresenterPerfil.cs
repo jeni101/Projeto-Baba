0330b37e43e67b2fb05bd0550cdcd77a6713a5e5
Luix <lugasoal@gmail.com>
2025-06-06 01:33:52 -0300

    00.6.0.0.0.0.0.021

--- FurApp/Views/PresenterPerfil.cs
+++ FurApp/Views/PresenterPerfil.cs
@@ -13,19 +13,20 @@ namespace Presentation.Perfil
                 Console.WriteLine("Erro: Não foi possível exibir o perfil. DTO nulo");
                 return;
             }
-
-            Console.WriteLine("\n========================================");
-            Console.WriteLine($"=== PERFIL DE {perfilDTO.Nome.ToUpper()} ===");
-            Console.WriteLine("========================================");
-            Console.WriteLine($"ID: {perfilDTO.Id}");
-            Console.WriteLine($"Tipo: {perfilDTO.TipoConta}");
-            Console.WriteLine($"Idade: {perfilDTO.Idade} anos");
-            Console.WriteLine($"Saldo: R$ {perfilDTO.Saldo:F2}");
-            Console.WriteLine($"Membro desde: {perfilDTO.DataCriacao:dd/MM/yyyy}");
-            Console.WriteLine($"Interesses: {perfilDTO.Interesses}");
-            Console.WriteLine($"Amistosos: {perfilDTO.Amistosos}");
-            Console.WriteLine($"Time Associado: {perfilDTO.TimeAssociado}");
-            Console.WriteLine("========================================\n");
+                //Tem que testar ainda, provavelmente está bem desorganizado no print, Se alguem puder deixar um jeito de eu ver como fica no program eu agradeço
+            Console.WriteLine($" .________________________ Perfil De: ________________________.");
+            Console.WriteLine($" | -=-             {perfilDTO.Nome.ToUpper()}             -=- |");
+            Console.WriteLine($" |============================================================|");
+            Console.WriteLine($" |- ID: {perfilDTO.Id}                                        |");
+            Console.WriteLine($" |- Tipo: {perfilDTO.TipoConta}                               |");
+            Console.WriteLine($" |- Idade: {perfilDTO.Idade} anos                             |");
+            Console.WriteLine($" |- Saldo: R$ {perfilDTO.Saldo:F2}                            |");
+            Console.WriteLine($" |- Membro desde: {perfilDTO.DataCriacao:dd/MM/yyyy}          |");
+            Console.WriteLine($" |- Interesses: {perfilDTO.Interesses}                        |");
+            Console.WriteLine($" |- Amistosos: {perfilDTO.Amistosos}                          |");
+            Console.WriteLine($" |- Time Associado: {perfilDTO.TimeAssociado}                 |");
+            Console.WriteLine($" |____________________________________________________________|");
+            Console.WriteLine($" |============================================================|");
         }
     }
 }
--- FurApp/Views/Views_Partidas.cs
+++ FurApp/Views/Views_Partidas.cs
@@ -6,61 +6,188 @@ namespace Views.Partidas
 {
     public class Views_Partidas
     {
-        public async Task Display_InfoPartidas()
+        public async Task Display_InfoPartidas_22()
         {
             int[] validos = { 1, 2 };
             bool sair = false;
             int contador_de_erros = 0;
 
-            while (!sair) 
+            while (!sair)
             {
-                Console.Clear();
-                View_Inicial.Display_Mascara01();
+                Console.Clear();                         //  Aqui precisa fazer Bastante coisa, Como por exemplo:
+                View_Inicial.Display_Mascara01();        //    Adicionar Times no Rodapé, Placar Automático, Listar cada jogador e Linkar funções dos botões
                 Console.WriteLine(" .____________________ Placar ____________________.");
-                Console.WriteLine(" |-=-    Time A        7  X  1     Time B      -=-|");
-                Console.WriteLine(" |========================|=======================|   . ._______________________________.    ");
-                Console.WriteLine(" |                        |                       |     |-      |  |         |  |      -|    ");
-                Console.WriteLine(" |                        |                       |  .  |       |  ˙‾‾‾‾-‾‾‾‾˙  |       |    ");
-                Console.WriteLine(" |                        |                       |     |       ˙‾‾‾‾‾-----‾‾‾‾‾˙       |    ");
-                Console.WriteLine(" |                        |                       |   . |                               |    ");
-                Console.WriteLine(" |                        |                       |     |                               |    ");
-                Console.WriteLine(" |                        |                       | .   |            __---__            |    ");
-                Console.WriteLine(" |                        |                       |     |           |   |   |           |    ");
-                Console.WriteLine(" |                        |                       |   . |==========|====-====|==========|    ");
-                Console.WriteLine(" |                        |                       |     |           |   |   |           |    ");
-                Console.WriteLine(" |                        |                       | .   |            ‾‾---‾‾            |    ");
-                Console.WriteLine(" |                        |                       |     |                               |    ");
-                Console.WriteLine(" |                        |                       |    .|                               |    ");
-                Console.WriteLine(" |________________________|_______________________|     |       ._____-----_____.       |    ");
-                Console.WriteLine(" |- SAIR  . . . . . . . . . . . . . . . . . |  0  |     |       |  .____-____.  |       |    ");
-                Console.WriteLine(" |================================================|  .  |-      |  |         |  |      -|    ");
-                Console.WriteLine("  .          .              .        .        .     .   ˙‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾˙  . ");
+                Console.WriteLine(" |-=-    Time A        0  X  0     Time B      -=-|");
+                Console.WriteLine(" |========================|=======================|   . ._______________________________.  . ");
+                Console.WriteLine(" | - Tecnico              | - Tecnico             |     |-      |  |         |  |      -|    ");
+                Console.WriteLine(" | - jogador 1            | - Jogador 1           |  .  |       |  ˙‾‾‾‾-‾‾‾‾˙  |       |    ");
+                Console.WriteLine(" | - jogador 2            | - Jogador 2           |     |       ˙‾‾‾‾‾-----‾‾‾‾‾˙       |.   ");
+                Console.WriteLine(" | - jogador 3            | - Jogador 3           |   . |                               |    ");
+                Console.WriteLine(" | - jogador 4            | - Jogador 4           |     |                               |    ");
+                Console.WriteLine(" | - jogador 5            | - Jogador 5           | .   |            __---__            |    ");
+                Console.WriteLine(" | - jogador 6            | - Jogador 6           |     |           |       |           |    ");
+                Console.WriteLine(" | - jogador 7            | - Jogador 7           |   . |==========|=== - ===|==========|  . ");
+                Console.WriteLine(" | - jogador 8            | - Jogador 8           |     |           |       |           |    ");
+                Console.WriteLine(" | - jogador 9            | - Jogador 9           | .   |            ‾‾---‾‾            |    ");
+                Console.WriteLine(" | - jogador 10           | - Jogador 10          |     |                               |    ");
+                Console.WriteLine(" | - jogador 11           | - Jogador 11          |    .|                               |    ");
+                Console.WriteLine(" |================================================|     |       ._____-----_____.       | .  ");
+                Console.WriteLine(" .-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=.     |       |  .____-____.  |       |    ");
+                Console.WriteLine(" |- Adicionar Gol . |  1  |-                |  2  |  .  |-      |  |         |  |      -|    ");
+                Console.WriteLine(" |==================|=====|=================|=====| .   ˙‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾˙  . ");
+                Console.WriteLine(" |- Encerrar  . . . |  3  |- Voltar . . . . |  0  |              .         .        .        ");
+                Console.WriteLine(" |================================================| .       .            .              .    ");
                 Console.WriteLine(" • Digite a Opção Desejada: ");
                 string? escolha = Console.ReadLine();
 
-               var HouveErro = await ControleDeExecoes.ExecutarComTratamento(async () =>
-                {
-                    int opcao = int.Parse(escolha ?? "");
-                    switch (opcao)
-                    {
-                        case 1:
+                var HouveErro = await ControleDeExecoes.ExecutarComTratamento(async () =>
+                 {
+                     int opcao = int.Parse(escolha ?? "");
+                     switch (opcao)
+                     {
+                         case 1:
+                             //linkar Função de Adicionar Gol
+                             break;
 
-                            break;
+                         case 2:
+                             //Linkar Função Extra
+                             break;
 
-                        case 2:
+                         case 3:
+                             //Linkar Função de Encerrar Partida
+                             break;
 
-                            break;
+                         case 0:
+                             Confirmacao.ExibirMensagemVoltando(ref opcao);
+                             sair = true;
+                             break;
 
-                        case 0:
-                            Confirmacao.ExibirMensagemSaida(ref opcao);
-                            sair = true;
-                            break;
+                         default:
+                             throw new ArgumentOutOfRangeException();
+                     }
+                 }, escolha ?? "", contador_de_erros);
+                if (sair) break;
+            }
+        }
+        public async Task Display_InfoPartidas_14()
+        {
+            int[] validos = { 1, 2 };
+            bool sair = false;
+            int contador_de_erros = 0;
 
-                        default:
-                            throw new ArgumentOutOfRangeException();
-                    }
-                }, escolha ?? "", contador_de_erros);
-                if (sair) break; 
+            while (!sair)
+            {
+                Console.Clear();                         //  Aqui precisa fazer Bastante coisa, Como por exemplo:
+                View_Inicial.Display_Mascara01();        //    Adicionar Times no Rodapé, Placar Automático, Listar cada jogador e Linkar funções dos botões
+                Console.WriteLine(" .____________________ Placar ____________________.");
+                Console.WriteLine(" |-=-    Time A        0  X  0     Time B      -=-|");
+                Console.WriteLine(" |========================|=======================|   . ._______________________________.  . ");
+                Console.WriteLine(" | - Tecnico              | - Tecnico             |     |-      |  |         |  |      -|    ");
+                Console.WriteLine(" | - jogador 1            | - Jogador 1           |  .  |       |  ˙‾‾‾‾-‾‾‾‾˙  |       |    ");
+                Console.WriteLine(" | - jogador 2            | - Jogador 2           |     |       ˙‾‾‾‾‾-----‾‾‾‾‾˙       |.   ");
+                Console.WriteLine(" | - jogador 3            | - Jogador 3           |   . |                               |    ");
+                Console.WriteLine(" | - jogador 4            | - Jogador 4           |     |                               |    ");
+                Console.WriteLine(" | - jogador 5            | - Jogador 5           | .   |            __---__            |    ");
+                Console.WriteLine(" | - jogador 6            | - Jogador 6           |     |           |       |           |    ");
+                Console.WriteLine(" | - jogador 7            | - Jogador 7           |   . |==========|=== - ===|==========|  . ");
+                Console.WriteLine(" |                        |                       |     |           |       |           |    ");
+                Console.WriteLine(" |================================================| .   |            ‾‾---‾‾            |    ");
+                Console.WriteLine(" .-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=.     |                               |    ");
+                Console.WriteLine(" |- Adicionar Gol . |  1  |-                |  2  |    .|                               |    ");
+                Console.WriteLine(" |==================|=====|=================|=====|     |       ._____-----_____.       | .  ");
+                Console.WriteLine(" |- Encerrar  . . . |  3  |- Voltar . . . . |  0  |     |       |  .____-____.  |       |    ");
+                Console.WriteLine(" |================================================|  .  |-      |  |         |  |      -|    ");
+                Console.WriteLine("                                                    .   ˙‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾˙  . ");
+                Console.WriteLine(" • Digite a Opção Desejada:                                      .         .        .        ");
+                string? escolha = Console.ReadLine();
+
+                var HouveErro = await ControleDeExecoes.ExecutarComTratamento(async () =>
+                 {
+                     int opcao = int.Parse(escolha ?? "");
+                     switch (opcao)
+                     {
+                         case 1:
+                             //linkar Função de Adicionar Gol
+                             break;
+
+                         case 2:
+                             //Linkar Função Extra
+                             break;
+
+                         case 3:
+                             //Linkar Função de Encerrar Partida
+                             break;
+
+                         case 0:
+                             Confirmacao.ExibirMensagemVoltando(ref opcao);
+                             sair = true;
+                             break;
+
+                         default:
+                             throw new ArgumentOutOfRangeException();
+                     }
+                 }, escolha ?? "", contador_de_erros);
+                if (sair) break;
+            }
+        }
+        public async Task Display_InfoPartidas_10()
+        {
+            int[] validos = { 1, 2 };
+            bool sair = false;
+            int contador_de_erros = 0;
+
+            while (!sair)
+            {
+                Console.Clear();                         //  Aqui precisa fazer Bastante coisa, Como por exemplo:
+                View_Inicial.Display_Mascara01();        //    Adicionar Times no Rodapé, Placar Automático, Listar cada jogador e Linkar funções dos botões
+                Console.WriteLine(" .____________________ Placar ____________________.");
+                Console.WriteLine(" |-=-    Time A        0  X  0     Time B      -=-|");
+                Console.WriteLine(" |========================|=======================|   . ._______________________________.  . ");
+                Console.WriteLine(" | - Tecnico              | - Tecnico             |     |-      |  |         |  |      -|    ");
+                Console.WriteLine(" | - jogador 1            | - Jogador 1           |  .  |       |  ˙‾‾‾‾-‾‾‾‾˙  |       |    ");
+                Console.WriteLine(" | - jogador 2            | - Jogador 2           |     |       ˙‾‾‾‾‾-----‾‾‾‾‾˙       |.   ");
+                Console.WriteLine(" | - jogador 3            | - Jogador 3           |   . |                               |    ");
+                Console.WriteLine(" | - jogador 4            | - Jogador 4           |     |                               |    ");
+                Console.WriteLine(" | - jogador 5            | - Jogador 5           | .   |            __---__            |    ");
+                Console.WriteLine(" |                        |                       |     |           |       |           |    ");
+                Console.WriteLine(" |================================================|   . |==========|=== - ===|==========|  . ");
+                Console.WriteLine(" .-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=.     |           |       |           |    ");
+                Console.WriteLine(" |- Adicionar Gol . |  1  |-                |  2  | .   |            ‾‾---‾‾            |    ");
+                Console.WriteLine(" |==================|=====|=================|=====|     |                               |    ");
+                Console.WriteLine(" |- Encerrar  . . . |  3  |- Voltar . . . . |  0  |    .|                               |    ");
+                Console.WriteLine(" |================================================|     |       ._____-----_____.       | .  ");
+                Console.WriteLine("                                                        |       |  .____-____.  |       |    ");
+                Console.WriteLine("  • Digite a Opção Desejada:                         .  |-      |  |         |  |      -|    ");
+                Console.WriteLine("                                                    .   ˙‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾˙  . ");
+                string? escolha = Console.ReadLine();
+
+                var HouveErro = await ControleDeExecoes.ExecutarComTratamento(async () =>
+                 {
+                     int opcao = int.Parse(escolha ?? "");
+                     switch (opcao)
+                     {
+                         case 1:
+                            //linkar Função de Adicionar Gol
+                             break;
+
+                         case 2:
+                            //Linkar Função Extra
+                             break;
+
+                         case 3:
+                            //Linkar Função de Encerrar Partida
+                             break;
+
+                         case 0:
+                             Confirmacao.ExibirMensagemVoltando(ref opcao);
+                             sair = true;
+                             break;
+
+                         default:
+                             throw new ArgumentOutOfRangeException();
+                     }
+                 }, escolha ?? "", contador_de_erros);
+                if (sair) break;
             }
         }
     }
